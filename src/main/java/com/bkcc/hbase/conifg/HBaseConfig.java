d26bfcaa242065c356a051b9d918a52fd088ca9e
chenruhan <339794349@qq.com>
2020-02-16 15:50:35 +0800

    hbase启用

--- src/main/java/com/bkcc/hbase/conifg/HBaseConfig.java
+++ src/main/java/com/bkcc/hbase/conifg/HBaseConfig.java
@@ -20,40 +20,40 @@ import org.springframework.data.hadoop.hbase.HbaseTemplate;
  *  @version        v1.0 Jun 25, 2019 新建
  *  @since          Jun 25, 2019 
  */
-//@Configuration
-//@EnableConfigurationProperties(HBaseProperties.class)
+@Configuration
+@EnableConfigurationProperties(HBaseProperties.class)
 public class HBaseConfig {
 	
-//	private final HBaseProperties properties;
-//
-//    public HBaseConfig(HBaseProperties properties) {
-//        this.properties = properties;
-//    }
-//
-//    @Bean
-//    public HbaseTemplate hbaseTemplate() {
-//        HbaseTemplate hbaseTemplate = new HbaseTemplate();
-//        hbaseTemplate.setConfiguration(configuration());
-//        hbaseTemplate.setAutoFlush(true);
-//        return hbaseTemplate;
-//    }
-//
-//    @Bean
-//    public org.apache.hadoop.conf.Configuration configuration() {
-//        org.apache.hadoop.conf.Configuration configuration = HBaseConfiguration.create();
-//        Map<String, String> config = properties.getConfig();
-//        Set<String> keySet = config.keySet();
-//        for (String key : keySet) {
-//            configuration.set(key, config.get(key));
-//        }
-//        return configuration;
-//    }
-//
-//
-//    @Bean
-//    public Connection connection() throws IOException {
-//    	Connection connection = ConnectionFactory.createConnection(configuration());
-//		return connection;
-//	}
+	private final HBaseProperties properties;
+
+    public HBaseConfig(HBaseProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public HbaseTemplate hbaseTemplate() {
+        HbaseTemplate hbaseTemplate = new HbaseTemplate();
+        hbaseTemplate.setConfiguration(configuration());
+        hbaseTemplate.setAutoFlush(true);
+        return hbaseTemplate;
+    }
+
+    @Bean
+    public org.apache.hadoop.conf.Configuration configuration() {
+        org.apache.hadoop.conf.Configuration configuration = HBaseConfiguration.create();
+        Map<String, String> config = properties.getConfig();
+        Set<String> keySet = config.keySet();
+        for (String key : keySet) {
+            configuration.set(key, config.get(key));
+        }
+        return configuration;
+    }
+
+
+    @Bean
+    public Connection connection() throws IOException {
+    	Connection connection = ConnectionFactory.createConnection(configuration());
+		return connection;
+	}
     
 }
